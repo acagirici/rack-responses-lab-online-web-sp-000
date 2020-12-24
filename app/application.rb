a173acfd5e0fc24f3e28878e98c2036cbf7366d6
Arif Cagirici <acagirici23@gmail.com>
2020-12-24 01:15:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,13 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    num_1 = Kernel.rand (1..12)
+    num_1 = Kernel.rand (1,12)
     num_2 = Kernel.rand (13..24)
     
       resp.write "#{num_1}\n"
       resp.write "#{num_2}\n"
       
-    case
+    case 
     when num_1
       resp.write "Good Morning!"
     when num_2
